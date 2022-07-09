8c7947ed541482083a8953e8a1acfe48f79b17e0
Vasyl Ulianovskiy <vasia.kit434@gmail.com>
2022-07-09 23:05:08 +0300

    React

---
+++ app/src/App/app.js
@@ -0,0 +1,14 @@
+import Card from '../Card/card';
+import './Style.css';
+
+
+function OnlineStore() {
+    return (
+        <div>
+            <Card />
+            
+        </div>
+    );
+}
+
+export default OnlineStore;
